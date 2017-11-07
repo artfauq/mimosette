548fafe1f62544fa4bf8f6971b0932eb6cbdfa5a
Arthur Fauquenot <arthurfauquenot@yahoo.com>
2017-11-07 11:43:18 +0100

    Rename /app folder to /src, add ESLint, fix missing dependencie and modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,71 @@
-var gulp = require("gulp"),
-  gutil = require("gulp-util"),
-  gulpif = require("gulp-if"),
-  clean = require("gulp-clean"),
-  cleanCSS = require("gulp-clean-css"),
-  critical = require("critical").stream,
-  purge = require("gulp-css-purge"),
-  watch = require("gulp-watch"),
-  useref = require("gulp-useref"),
-  browserSync = require("browser-sync").create(),
-  sass = require("gulp-sass"),
-  jshint = require("gulp-jshint"),
-  uglify = require("gulp-uglify"),
-  imagemin = require("gulp-imagemin"),
-  autoprefixer = require("gulp-autoprefixer"),
-  runSequence = require("run-sequence");
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const clean = require('gulp-clean')
+const cleanCSS = require('gulp-clean-css')
+const critical = require('critical').stream
+const purge = require('gulp-css-purge')
+const useref = require('gulp-useref')
+const eslint = require('gulp-eslint')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+const autoprefixer = require('gulp-autoprefixer')
+const runSequence = require('run-sequence')
 
 var bases = {
-  app: "./app/",
-  dist: "./dist/"
-};
+  app: './src/',
+  dist: './dist/'
+}
 
 var paths = {
-  css: "./css/**/*.css",
-  fonts: "./fonts/**/*.*",
-  html: "./index.html",
-  images: "./images/**/*.*",
+  css: './css/**/*.css',
+  fonts: './fonts/**/*.*',
+  html: './*.html',
+  images: './images/**/*.*',
   others: [
-    "*.xml",
-    "*.json",
-    ".htaccess",
-    "google46e54d9618da9e3c.html",
-    "*.txt"
+    '*.xml',
+    '*.json',
+    '.htaccess',
+    '*.txt'
   ],
-  scripts: "./js/**/*.js",
-  scss: "./styles/scss/**/*.scss"
-};
+  scripts: './js/**/*.js',
+  scss: './styles/scss/**/*.scss'
+}
 
-gulp.task("jshint", function() {
+gulp.task('eslint', function () {
   gulp
     .src(paths.scripts, {
       cwd: bases.app
     })
-    .pipe(jshint())
-    .pipe(jshint.reporter("jshint-stylish"));
-});
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+})
 
-gulp.task("sass", function() {
+gulp.task('sass', function () {
   return gulp
     .src(paths.scss, {
       cwd: bases.app
     })
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest(bases.app + "/styles/css/"));
-});
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(bases.app + '/styles/css/'))
+})
 
-gulp.task("scripts", function() {
+gulp.task('scripts', function () {
   return gulp
     .src(paths.scripts, {
       cwd: bases.dist
     })
     .pipe(uglify())
     .pipe(
-      gulp.dest("js/", {
+      gulp.dest('js/', {
         cwd: bases.dist
       })
-    );
-});
+    )
+})
 
-gulp.task("styles", function() {
+gulp.task('styles', function () {
   return gulp
     .src(paths.css, {
       cwd: bases.dist
@@ -76,15 +74,15 @@ gulp.task("styles", function() {
     .pipe(purge())
     .pipe(cleanCSS())
     .pipe(
-      gulp.dest("css/", {
+      gulp.dest('css/', {
         cwd: bases.dist
       })
-    );
-});
+    )
+})
 
-gulp.task("critical", function() {
+gulp.task('critical', function () {
   return gulp
-    .src("index.html", {
+    .src('index.html', {
       cwd: bases.dist
     })
     .pipe(
@@ -92,87 +90,86 @@ gulp.task("critical", function() {
         base: bases.dist,
         inline: true,
         minify: true,
-        css: [bases.dist + "./css/main.min.css"]
+        css: [bases.dist + './css/main.min.css']
       })
     )
-    .on("error", function(err) {
-      gutil.log(gutil.colors.red(err.message));
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red(err.message))
     })
-    .pipe(gulp.dest(bases.dist));
-});
+    .pipe(gulp.dest(bases.dist))
+})
 
 // Copy tasks
-gulp.task("fonts", function() {
+gulp.task('fonts', function () {
   return gulp
     .src(paths.fonts, {
       cwd: bases.app
     })
-    .pipe(gulp.dest(bases.dist + "/fonts"));
-});
+    .pipe(gulp.dest(bases.dist + '/fonts'))
+})
 
-gulp.task("html", function() {
+gulp.task('html', function () {
   return gulp
     .src(paths.html, {
       cwd: bases.app
     })
     .pipe(useref())
-    .pipe(gulp.dest(bases.dist));
-});
+    .pipe(gulp.dest(bases.dist))
+})
 
-gulp.task("images", function() {
+gulp.task('images', function () {
   return gulp
     .src(paths.images, {
       cwd: bases.app
     })
     .pipe(imagemin())
-    .pipe(gulp.dest(bases.dist + "/images"));
-});
+    .pipe(gulp.dest(bases.dist + '/images'))
+})
 
-gulp.task("others", function() {
+gulp.task('others', function () {
   return gulp
     .src(paths.others, {
       cwd: bases.app
     })
-    .pipe(gulp.dest(bases.dist));
-});
+    .pipe(gulp.dest(bases.dist))
+})
 
 // Clean task
-gulp.task("clean:dist", function() {
+gulp.task('clean:dist', function () {
   return gulp
     .src(bases.dist, {
       read: false
     })
-    .pipe(clean());
-});
+    .pipe(clean())
+})
 
 // Build task
-gulp.task("build", function(cb) {
+gulp.task('build', function (cb) {
   runSequence(
-    "clean:dist",
-    ["sass", "jshint"],
-    ["html", "fonts", "others", "images"],
-    ["styles", "scripts"],
-    "critical",
-    "serve:dist",
+    'clean:dist', ['sass', 'eslint'], ['html', 'fonts', 'others', 'images'], ['styles', 'scripts'],
+    'critical',
+    'serve:dist',
     cb
-  );
-});
+  )
+})
 
 // Serve tasks
-gulp.task("serve", function() {
+gulp.task('serve', ['sass', 'eslint'], function () {
   browserSync.init({
-    server: "app/",
-    port: 8080
-  });
+    server: {
+      baseDir: ['./', bases.app]
+    }
+  })
 
-  gulp.watch(bases.app + paths.html).on("change", browserSync.reload);
-  gulp.watch(bases.app + paths.scss, ["sass"]).on("change", browserSync.reload);
-  gulp.watch(bases.app + paths.scripts).on("change", browserSync.reload);
-});
+  gulp.watch(bases.app + paths.html).on('change', browserSync.reload)
+  gulp.watch(bases.app + paths.scss, ['sass']).on('change', browserSync.reload)
+  gulp.watch(bases.app + paths.scripts, ['eslint']).on('change', browserSync.reload)
+})
 
-gulp.task("serve:dist", function() {
+gulp.task('serve:dist', function () {
   browserSync.init({
-    server: "dist/",
-    port: 8080
-  });
-});
+    server: {
+      baseDir: bases.dist
+    }
+  })
+})
---
+++ src/js/maps.js
@@ -0,0 +1,20 @@
+/* global google */
+/* eslint no-unused-vars: "off" */
+
+function loadMap () {
+  var myLatLng = {
+    lat: 47.018927,
+    lng: -2.245554
+  }
+
+  var map = new google.maps.Map(document.getElementById('maps'), {
+    zoom: 11,
+    center: myLatLng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  })
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map
+  })
+}
